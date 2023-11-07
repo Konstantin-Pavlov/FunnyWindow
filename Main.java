9bc6bcc614dc4d4515f5eae765f5d7dbff0d2bba
Konstantin-Pavlov <konstpavlov87@gmail.com>
2023-11-07 19:46:17 +0600

    added functionality to make the window appear in the center of the screen

--- Main.java
+++ Main.java
@@ -1,10 +1,28 @@
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import javax.swing.WindowConstants;
 
 public class Main {
     public static void main(String[] args) {
         Gui gui = new Gui();
-        gui.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        gui.setSize(600, 400);
+
+        // get screen size
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+        // Set the window size
+        int width = 600;
+        int height = 400;
+
+        // Calculate the X and Y coordinates to center the window
+        int x = (screenSize.width - width) / 2;
+        int y = (screenSize.height - height) / 2;
+
+        gui.setSize(width, height);
+        gui.setLocation(x, y);
+
+        // Make the window visible
         gui.setVisible(true);
+        gui.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 }
